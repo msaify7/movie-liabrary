d291bcb678a6693a9b3cff89f21f2eed8a917195
Mustafa Saify <mustafasaify456@gmail.com>
2024-10-11 20:06:57 +0530

    fixed heading typo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ function Header(props) {
   return (
     <div className="head">
       <div className="logo">
-        <NavLink to="/">Movie Library</NavLink>
+        <NavLink to="/">Movie Liabrary</NavLink>
       </div>
       <div className="search">
         <input
